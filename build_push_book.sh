3094e18000f5e40c800e23161a1edd45dbd0a480
richardye101 <richardyeee@outlook.com>
2023-02-05 23:23:28 -0500

    updated book

---
+++ build_push_book.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+cd book/
+# fix image links in obsidian
+find Notes/ -type f -name '*.md' -exec gsed -i 's+\!\[\[STAD70\/book/+!\[\](+g' {} \;
+find Notes/ -type f -name '*.md' -exec gsed -i 's+.png\]\]+.png)+g' {} \;
+
+# build book 
+R --quiet -e 'rmarkdown::render_site(encoding = 'UTF-8')'
+cd ../
+
+touch book/docs/.nojekyll
+rm -rf docs
+cp -r book/docs .
+git add .
+git commit -m "updated book"
+git push
--- update_gh_pages.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-touch book/docs/.nojekyll
-rm -rf docs
-cp -r book/docs .
-git add .
-git commit -m "updated book"
-git push
